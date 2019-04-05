7d6b46239b5f2b191eb4350a4b4f0775fd970574
Alaa <alaa.hassanein.ali@gmail.com>
2019-04-05 06:27:44 -0700

    submit homework_04

---
+++ homework_04/excercise1.js
@@ -0,0 +1,28 @@
+const rxjs= require('rxjs');
+const os = require('os');
+
+   // using Observable 
+   const { Observable } = rxjs;
+   const obj$ = Observable.create(
+       function(observer){           
+            observer.next("Checking your system..");  
+            if (os.freemem() < 4294967296) { 
+                observer.error("This app needs at least 4 GB of RAM.");
+        
+            } 
+            else if (os.cpus().length < 2) {
+                observer.error("Processor is not supported.");        
+            } 
+           observer.complete();
+       }
+   );
+   const subscription = obj$.subscribe( 
+       (res) => console.log(res),
+       (err) => console.log(err),
+       ()=>console.log('System is checked successfully.')  
+       );
+
+
+
+       
+  
